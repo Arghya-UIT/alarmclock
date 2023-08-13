aec69eba5631c5179123de1f977a23f50d1d674e
Arghya ghosh <milfo456kian@gmail.com>
2023-08-13 12:06:16 +0530

    some changes

--- Alarmclock/app/src/main/java/com/example/alarmclock/CreateAlarm.java
+++ Alarmclock/app/src/main/java/com/example/alarmclock/CreateAlarm.java
@@ -1,6 +1,10 @@
 package com.example.alarmclock;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +13,22 @@ public class CreateAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_alarm);
+
+
+        LinearLayout getTime = findViewById(R.id.getTimeLayout);
+        LinearLayout getSelectedDays = findViewById(R.id.getSelectedDaysLayout);
+        LinearLayout getRingtoneUri = findViewById(R.id.getRingtoneLayout);
+        EditText getAlarmName = findViewById(R.id.getAlarmNameTextview);
+        Button cancelButton= findViewById(R.id.cancelButton);
+        Button saveButton= findViewById(R.id.saveButton);
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
 
     }
--- Alarmclock/app/src/main/java/com/example/alarmclock/database/AlarmModel.java
+++ Alarmclock/app/src/main/java/com/example/alarmclock/database/AlarmModel.java
@@ -4,5 +4,10 @@ public class AlarmModel {
 
     private int id;
     private String alarm_name;
+    private String time_for_display;
+    private int time_for_store;
+    private String[] selected_days;
+    private String ringtone_uri;
+
 
 }
