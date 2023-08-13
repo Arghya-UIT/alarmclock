3be7e47a819c3ebbcd7614fee0cfbbd50aecf80d
Arghya ghosh <milfo456kian@gmail.com>
2023-08-13 13:11:16 +0530

    code corrected

--- Alarmclock/app/src/main/java/com/example/alarmclock/CreateAlarm.java
+++ Alarmclock/app/src/main/java/com/example/alarmclock/CreateAlarm.java
@@ -5,31 +5,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 public class CreateAlarm extends AppCompatActivity {
+
+    private LinearLayoutCompat getTimeLayout;
+    private LinearLayoutCompat getSelectedDaysLayout;
+    private LinearLayoutCompat getRingtoneLayout;
+    private EditText getAlarmNameTextview;
+    private Button cancelButton;
+    private Button saveButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_alarm);
 
+        // Initialize UI elements
+        getTimeLayout = findViewById(R.id.getTimeLayout);
+        getSelectedDaysLayout = findViewById(R.id.getSelectedDaysLayout);
+        getRingtoneLayout = findViewById(R.id.getRingtoneLayout);
+        getAlarmNameTextview = findViewById(R.id.getAlarmNameTextview);
+        cancelButton = findViewById(R.id.cancelButton);
+        saveButton = findViewById(R.id.saveButton);
 
-        LinearLayout getTime = findViewById(R.id.getTimeLayout);
-        LinearLayout getSelectedDays = findViewById(R.id.getSelectedDaysLayout);
-        LinearLayout getRingtoneUri = findViewById(R.id.getRingtoneLayout);
-        EditText getAlarmName = findViewById(R.id.getAlarmNameTextview);
-        Button cancelButton= findViewById(R.id.cancelButton);
-        Button saveButton= findViewById(R.id.saveButton);
-
-
+        // Set click listeners for buttons
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                finish();
+            public void onClick(View v) {
+                // Handle cancel button click
+                finish(); // Close the activity
             }
         });
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle save button click
 
+                finish(); // Close the activity
+            }
+        });
     }
 }
